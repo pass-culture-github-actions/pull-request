e858f4a1b944d91cd040811b24631694c8117d7c
Dimitri Kopriwa <kopx@free.fr>
2024-01-12 17:15:53 +0100

    first commit

--- README.md
+++ README.md
@@ -14,22 +14,22 @@ This action will retrieve PR informations.
 - name: Get PR information
   id: pr
   uses: pass-culture-github-actions/pull-request@v1.0.0
-- run: echo url="${{ steps.pr.outputs.url }}"
-- run: echo title="${{ steps.pr.outputs.title }}"
 - run: echo number="${{ steps.pr.outputs.number }}"
+- run: echo title="${{ steps.pr.outputs.title }}"
+- run: echo url="${{ steps.pr.outputs.url }}"
 
 ```
 
 ## Outputs
 
-### `url`
+### `number`
 
-The url of the PR
+The title of the PR
 
 ### `title`
 
 The title of the PR
 
-### `number`
+### `url`
 
-The title of the PR
+The url of the PR
--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const { exec } = require('child_process');
 try {
   const token = core.getInput('github-oauth-token');
   const { GITHUB_REPOSITORY, GITHUB_SHA } = process.env;
-  const command = `curl -s -H "Accept: application/vnd.github.groot-preview+json" -H "Authorization: token ${token}" https://api.github.com/repos/${ GITHUB_REPOSITORY }/commits/${ GITHUB_SHA }/pulls | jq -r '.[].number'`;
-  exec(command, (err, prNumber, stderr) => {
-    if (err) {
-      throw err;
+  const curlOptions = `-s -H "Accept: application/vnd.github.groot-preview+json" -H "Authorization: token ${token}"`
+  const commandPrNumber = `curl ${curlOptions} https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/pulls`;
+  exec(commandPrNumber, (errPulls, pulls, stderrPulls) => {
+    if (errPulls) {
+      throw errPulls;
     }
-    const url = prNumber && `https://github.com/${ GITHUB_REPOSITORY }/pull/${ prNumber.trim() }`;
-    core.setOutput("url", url);
+    const jsonPulls = JSON.parse(pulls);
+    if (jsonPulls && jsonPulls[0] && jsonPulls[0].number) {
+      const number = jsonPulls[0].number;
+
+      core.setOutput("number", number);
+      const commandPr = `curl ${curlOptions} https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${number}`;
+      exec(commandPr, (err, pr, stderr) => {
+        if (err) {
+          throw err;
+        }
+
+        const json = JSON.parse(pr);
+
+        core.setOutput("title", json.title);
+        core.setOutput("url", json.html_url);
+      })
+    }
   });
 } catch (error) {
   core.setFailed(error.message);
